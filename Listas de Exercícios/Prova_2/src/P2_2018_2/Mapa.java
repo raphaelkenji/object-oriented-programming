eb8c01a0d68ca9296bcc57788f46f6ab290f4fb6
Raphael Kenji <raphael.kenji@aluno.cefet-rj.br>
2023-12-01 01:30:52 -0300

    update

--- Listas de Exercícios/Prova_2/src/P2_2018_2/Mapa.java
+++ Listas de Exercícios/Prova_2/src/P2_2018_2/Mapa.java
@@ -1,14 +1,13 @@
 package P2_2018_2;
 
-import P2_2018_2.Doceria.CriarMapa;
-
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 public class Mapa {
     public static void main(String[] args) {
-        Map<String, Double> mapa = CriarMapa.devolveMapa();
+        Map<String, Double> mapa = devolveMapa();
         double soma = contaA(mapa);
         System.out.println(soma);
     }
@@ -24,5 +23,18 @@ public class Mapa {
             }
         }
         return soma;
+    }
+
+    public static Map<String, Double> devolveMapa() {
+        Map<String, Double> mapa = new HashMap<>();
+        mapa.put("A1", 5.0);
+        mapa.put("A2", 6.3);
+        mapa.put("A3", 7.0);
+        mapa.put("BD21", 3.0);
+        mapa.put("B2", 7.6);
+        mapa.put("AC74", 2.0);
+        mapa.put("A5", 8.0);
+        mapa.put("B3", 3.0);
+        return mapa;
     }
 }
